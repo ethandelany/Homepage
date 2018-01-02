2cfdde8f861da33e47f232f6fd228af9bb519a14
StarryMuffin <34615968+StarryMuffin@users.noreply.github.com>
2018-01-01 22:48:26 -0700

    Testing PHP

---
+++ contact.php
@@ -0,0 +1,14 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>Contact - Ethan Delany</title>
+<link href="contact.css" rel="stylesheet" type="text/css">
+</head>
+
+<body>
+  Hi <?php
+  echo htmlspecialchars($_PUT['fname']);
+  ?>
+</body>
+</html>
